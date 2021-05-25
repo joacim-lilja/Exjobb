c44f8fb6e616630d766c022ba4957aa620240a8b
Joacim Lilja <lijo1729@student.ju.se>
2021-05-25 21:12:35 +0200

    added gyroscope almost working

--- GPS/App.js
+++ GPS/App.js
@@ -9,24 +9,24 @@
 import * as React from 'react';
 import type { Node } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Button,
-  PermissionsAndroid,
-  NativeEventEmitter,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Button,
+    PermissionsAndroid,
+    NativeEventEmitter,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 // Navigation
@@ -45,136 +45,141 @@ import { tsConstructorType, whileStatement } from '@babel/types';
 // Map
 
 import MapView, { AnimatedRegion, Animated } from 'react-native-maps';
+import { moveSyntheticComments } from 'typescript';
 
 // App
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: Colors.darker,
-  };
+    const backgroundStyle = {
+        backgroundColor: Colors.darker,
+    };
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="Map" component={MapScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{ title: 'Welcome' }}
+                />
+                <Stack.Screen name="Map" component={MapScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
 // Navigation screens
 //Home Screen
 
 const HomeScreen = ({ navigation }) => {
-  return (
-    <Button
-      title="Go to Map"
-      onPress={() => navigation.navigate('Map')}
-    />
-  );
+    return (
+        <Button
+            title="Go to Map"
+            onPress={() => navigation.navigate('Map')}
+        />
+    );
 };
 
 // Map Screen
 
 const MapScreen = ({ navigation, route }) => {
-  // var hasLocationPermission = true;
-  const [latitude, setLatitude] = React.useState(0);
-  const [longitude, setLongitude] = React.useState(0);
-  const [count, setCount] = React.useState(true);
-  const [centerOnUser, setCenterOnUser] = React.useState(false);
-  const [startLat, setStartLat] = React.useState(-1);
-  const [startLong, setStartLong] = React.useState(-1);
-  const [endLat, setEndLat] = React.useState(-1);
-  const [endLong, setEndLong] = React.useState(-1);
+    // var hasLocationPermission = true;
+    const [latitude, setLatitude] = React.useState(0);
+    const [longitude, setLongitude] = React.useState(0);
+    const [count, setCount] = React.useState(true);
+    const [centerOnUser, setCenterOnUser] = React.useState(false);
+    const [startLat, setStartLat] = React.useState(-1);
+    const [startLong, setStartLong] = React.useState(-1);
+    const [endLat, setEndLat] = React.useState(-1);
+    const [endLong, setEndLong] = React.useState(-1);
 
 
-  // Update coords on map on user movement
-  React.useEffect(() => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
-        setEndLat(position.coords.latitude);
-        setEndLong(position.coords.longitude);
-        console.log(position.coords.latitude, position.coords.longitude);
-      },
-      (error) => {
-        console.log(error);
-      },
-      { enableHighAccuracy: true }
-    );
-  }, [count]);
-  var myRegion = { latitude: latitude, longitude: longitude, latitudeDelta: 0.5, longitudeDelta: 0.3 };
-  console.log(myRegion);
-  // Returns View
-  return (
-    <View>
-      <Button title="Start measurement" onPress={() => {
-        setCenterOnUser(true);
-        setStartLat(latitude);
-        setStartLong(longitude);
-        console.log(startLat, startLong);
-      }} />
-      <Button title="Stop measurement" onPress={() => {
-        setCenterOnUser(false);
-        setEndLat(latitude);
-        setEndLong(longitude);
-        console.log(startLat, startLong);
-        console.log(endLat, endLong);
-      }} />
-      <MapView
-        region={myRegion}
-        style={{ top: 0, left: 0, height: 450 }}
-        showsUserLocation={true}
-        userLocationUpdateInterval={500}
-        onUserLocationChange={() => {
-          if (centerOnUser) {
-            setLatitude(latitude);
-            setLongitude(longitude);
-            setCount(!count);
-          }
-        }}
-      />
-      <Text>
-        Starting Coordinates:
-      </Text>
-      <Text>
-        Latitude: {startLat}       Longitude: {startLong}
-      </Text>
-      <Text>
-        Ending Coordinates:
-      </Text>
-      <Text>
-        Latitude: {endLat}        Longitude: {endLong}
-      </Text>
-    </View >
-  )
+    // Update coords on map on user movement
+    React.useEffect(() => {
+        Geolocation.getCurrentPosition(
+            (position) => {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);
+                setEndLat(position.coords.latitude);
+                setEndLong(position.coords.longitude);
+                console.log(position.coords.latitude, position.coords.longitude);
+            },
+            (error) => {
+                console.log(error);
+            },
+            { enableHighAccuracy: true }
+        );
+    }, [count]);
+    var myRegion = { latitude: latitude, longitude: longitude, latitudeDelta: 0.5, longitudeDelta: 0.3 };
+    console.log(myRegion);
+    // Returns View
+    return (
+        <View>
+            <View>
+                <Button title="Start measurement" onPress={() => {
+                    setCenterOnUser(true);
+                    setStartLat(latitude);
+                    setStartLong(longitude);
+                    console.log(startLat, startLong);
+                }} />
+                <Button title="Stop measurement" onPress={() => {
+                    setCenterOnUser(false);
+                    setEndLat(latitude);
+                    setEndLong(longitude);
+                    console.log(startLat, startLong);
+                    console.log(endLat, endLong);
+                }} />
+                <MapView
+                    region={myRegion}
+                    style={{ top: 0, left: 0, height: 450 }}
+                    showsUserLocation={true}
+                    userLocationUpdateInterval={500}
+                    onUserLocationChange={() => {
+                        if (centerOnUser) {
+                            setLatitude(latitude);
+                            setLongitude(longitude);
+                            setCount(!count);
+                        }
+                    }}
+                />
+            </View >
+            <View>
+                <Text>
+                    Starting Coordinates:
+                </Text>
+                <Text>
+                    Latitude: {startLat}       Longitude: {startLong}
+                </Text>
+                <Text>
+                    Ending Coordinates:
+                </Text>
+                <Text>
+                    Latitude: {endLat}        Longitude: {endLong}
+                </Text>
+            </View >
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
 
 });
 
--- INS/App.js
+++ INS/App.js
@@ -6,6 +6,12 @@
  * @flow strict-local
  */
 
+// 57.77839, 14.26678
+// 57.78839, 14.26678
+// skiljer 593m på 0.01 longitud => 59300m/longitud
+// skiljer 1110m på 0.01 latitud => 111000m/latitud
+
+
 import * as React from 'react';
 import type { Node } from 'react';
 import {
@@ -29,57 +35,40 @@ import {
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-// Navigation
 
+// Navigation
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-//Geolocation
 
+//Geolocation
 import Geolocation, { getCurrentPosition } from 'react-native-geolocation-service';
 import { tsConstructorType, whileStatement } from '@babel/types';
 
-// Map
 
+// Map
 import MapView, { AnimatedRegion, Animated } from 'react-native-maps';
 import { moveSyntheticComments } from 'typescript';
+
 
 // Sensors
 import {
     accelerometer,
     gyroscope,
+    magnetometer,
     setUpdateIntervalForType,
-    SensorTypes,
-    magnetometer
+    SensorTypes
 } from 'react-native-sensors';
 
-import { map, filter } from 'rxjs/operators';
+setUpdateIntervalForType(SensorTypes.accelerometer, 100);
+setUpdateIntervalForType(SensorTypes.gyroscope, 100);
 
-setUpdateIntervalForType(SensorTypes.accelerometer, 1000);
-setUpdateIntervalForType(SensorTypes.gyroscope, 1000);
-setUpdateIntervalForType(SensorTypes.magnetometer, 1000);
-
-// const subscriptiondeprecated = accelerometer
-//     .pipe(map(({ x, y, z }) => x + y + z), filter(speed => speed > 20))
-//     .subscribe(
-//         speed => console.log(`You moved your phone with ${speed}`),
-//         error => {
-//             console.log("The sensor is not available");
-//         }
-//     );
 
 // App
-
 const App: () => Node = () => {
-    const isDarkMode = useColorScheme() === 'dark';
-
-    const backgroundStyle = {
-        backgroundColor: Colors.darker,
-    };
-
     return (
         <NavigationContainer>
             <Stack.Navigator>
@@ -94,9 +83,9 @@ const App: () => Node = () => {
     );
 };
 
+
 // Navigation screens
 //Home Screen
-
 const HomeScreen = ({ navigation }) => {
     return (
         <Button
@@ -107,21 +96,31 @@ const HomeScreen = ({ navigation }) => {
 };
 
 // Map Screen
-
-const MapScreen = ({ navigation, route }) => {
+const MapScreen = () => {
+    // Coordinates
     const [latitude, setLatitude] = React.useState(0);
     const [longitude, setLongitude] = React.useState(0);
 
+    // Toggle for map update
     const [count, setCount] = React.useState(true);
 
+    //Center map on user
     const [centerOnUser, setCenterOnUser] = React.useState(false);
 
+    // Measured coordinates
     const [startLat, setStartLat] = React.useState(-1);
     const [startLong, setStartLong] = React.useState(-1);
     const [endLat, setEndLat] = React.useState(-1);
     const [endLong, setEndLong] = React.useState(-1);
 
+    //INS speed and distance
+    const [speed, setSpeed] = React.useState(0);
     const [distance, setDistance] = React.useState(0);
+
+    //INS rotation and angle
+    const [rotation, setRotation] = React.useState(0);
+    const [angle, setAngle] = React.useState(0);
+
 
     // Init coords
     React.useEffect(() => {
@@ -141,49 +140,27 @@ const MapScreen = ({ navigation, route }) => {
 
     // Update coords on map on user movement
     React.useEffect(() => {
+        // Subscribe to accelerometer data and measure speed and distance
         const accelSub = accelerometer.subscribe(({ x, y, z }) => {
-            console.log('accelerometer');
-            console.log("========XYZ=========");
-            console.log(x, y, z);
-            console.log("========XYZ=========");
-            console.log("====================");
-            console.log("====================");
-            setDistance(parseFloat(distance + x + y + z));
-            console.log("======distance======");
-            console.log(distance);
-            console.log("======distance======");
+            console.log(x.toFixed(2), y.toFixed(2), z.toFixed(2));
+            setSpeed(parseFloat((speed * 0.85 + (x + y + z - 9.82) * 0.15).toFixed(2)));
+            setDistance(parseFloat((distance + speed).toFixed(2)));
         });
 
-        // const gyroSub = gyroscope.subscribe(({ x, y, z, timestamp }) => {
-        //     setTimeout(() => {
-        //         console.log('gyroscope');
-        //         console.log({ x, y, z });
-        //     }, 1000);
-        // });
-        /*
-        Accelerometer summa = distans.
-        Gyrometer "snurr" = vinkel.
-
-        */
-
-        // const magneSub = magnetometer.subscribe(({ x, y, z, timestamp }) => {
-        //     setTimeout(() => {
-        //         console.log('magnet');
-        //         console.log({ x, y, z });
-        //     }, 1000);
-        // });
+        //Subscribe to gyroscope data and measure angle and rotation
+        const gyroSub = gyroscope.subscribe(({ x, y, z }) => {
+            setRotation(parseFloat(rotation*0.85 + z*0.15).toFixed(2));
+            console.log(rotation);
+            setAngle(parseFloat(angle + rotation).toFixed(2));
+            console.log(parseFloat(angle + rotation));
+        });
+        return () => {
+            accelSub.unsubscribe();
+            gyroSub.unsubscribe();
+        }
     }, [count]);
 
     var myRegion = { latitude: latitude, longitude: longitude, latitudeDelta: 0.5, longitudeDelta: 0.3 };
-    
-    React.useEffect(() => {
-        return () => {
-    setTimeout(() => {
-        accelSub.unsubscribe();
-        gyroSub.unsubscribe();
-    }, 1000);
-        }
-    }, [])
 
     // Returns View
     return (
@@ -203,12 +180,13 @@ const MapScreen = ({ navigation, route }) => {
                     region={myRegion}
                     style={{ top: 0, left: 0, height: 450 }}
                     showsUserLocation={true}
-                    userLocationUpdateInterval={1000}
+                    userLocationUpdateInterval={100}
                     onUserLocationChange={() => {
                         if (centerOnUser) {
                             setLatitude(latitude);
                             setLongitude(longitude);
                             setCount(!count);
+                            console.log('mapRender');
                         }
                     }}
                 />
@@ -227,6 +205,10 @@ const MapScreen = ({ navigation, route }) => {
                     Latitude: {endLat}        Longitude: {endLong}
                 </Text>
                 <Text>Distance: {distance}</Text>
+                <Text>Speed: {speed}</Text>
+                <Text>Rotation: {rotation}</Text>
+                <Text>Angle: {angle}</Text>
+                <Text>{centerOnUser ? 'measuring' : 'waiting'}</Text>
             </View >
         </View>
     )
